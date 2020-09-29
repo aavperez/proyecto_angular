19e496d9354eef43e4cefb092240cead9a65d2c8
Andrés Pérez <aavperez@gmail.com>
2020-09-29 19:35:25 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { XyzComponent } from './components/xyz/xyz.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XyzComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, XyzComponent]
 })
 export class AppModule { }
